9ec62438c38007f9f34abc72d4152dfa5208b9c3
joris carol <SkyWanker@MacBook-Pro-de-joris.local>
2017-03-14 06:44:00 +0100

    GNL data parsing

--- projects/getNextLine/get_next_line.c
+++ projects/getNextLine/get_next_line.c
@@ -9,3 +9,20 @@
 /*   Updated: 2017/03/03 05:33:23 by jocarol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int		ft_parse_data(char **data, char **line)
+{
+	char		*tmp;
+
+	tmp = NULL;
+	if (*data)
+	{
+		if ((tmp = ft_strchr((const char*)*data, '\n')))
+		{
+			*line = ft_strsub((char const*)*data, 0, tmp - *data);
+			ft_memmove(*data, tmp + 1, ft_strlen(tmp));
+		}
+	}
+}
--- projects/getNextLine/get_next_line.h
+++ projects/getNextLine/get_next_line.h
@@ -9,3 +9,17 @@
 /*   Updated: 2017/03/03 05:33:21 by jocarol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 1
+# define FD_VALUE 2147483647
+
+int get_next_line(int const fd, char **line);
+
+#endif
