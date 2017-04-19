59b505557bd71b23adc05ae04a76285d7621c8fe
joris carol <SkyWanker@Hal.local>
2017-04-19 15:19:26 +0200

    Updating parsing

--- projects/ft_ls/ft_ls.h
+++ projects/ft_ls/ft_ls.h
@@ -5,29 +5,29 @@
 
 #define bool int
 #define TRUE 1
-#define FALSE  0
+#define FALSE 0
 
-typedef struct		s_flag
+typedef struct			s_flag
 {
-	bool			all : 1;
-	bool			list: 1;
-	bool			rec : 1;
-	bool			rev :1;
-	bool			time : 1;
-}					t_flag;
+	bool				check;
+	bool				all;
+	bool				list;
+	bool				rec;
+	bool				rev;
+	bool				time;
+}						t_flag;
 
-typedef struct		s_element
+typedef struct			s_element
 {
-	bool			isFile;
-	bool			isDir;
-	int				type;
-	char			*path;
-	char			*inode;
-	char			*Name;
-}					t_element;
+	bool				isFile;
+	bool				isDir;
+	int					type;
+	char				*path;
+	char				*inode;
+	char				*Name;
+}						t_element;
 
-static void			ft_usage();
-static int			ft_checkflag(int c, char **av);
-t_flag				*ft_parse_flags(char **av);
+static void				ft_usage();
+t_flag					*ft_parseflags(int ac, char **av);
 
 #endif
--- projects/ft_ls/ft_ls_parse_debug.c
+++ projects/ft_ls/ft_ls_parse_debug.c
@@ -6,76 +6,54 @@ static void			ft_usage()
 {
 	ft_putstr_fd("Usage : ./ft_ls [-alRrt] [file][1]\n", 2);
 }
-   
-t_flag				*ft_parse_flags(char **av)
+
+static void			ft_fail(t_flag flag)
+{
+		free(flag);
+		ft_usage();
+		return (NULL);
+}
+
+t_flag				*ft_parseflags(int ac, char **av)
 {
 	int				i;
 	size_t			j;
+	int				sizeOfArg;
 	t_flag			*flag;
 
 	i = 0;
 	j = 0;
-	if (!(flag = (t_flag *)malloc(sizeof(*flag))))
-	{
-		ft_putstr_fd("E : Malloc failed.", 2);
+	sizeOfArg = ft_strlen(av[i]);
+	if (!(flag = ft_memalloc(sizeof(*flag))))
 		return (NULL);
-	}
-	else
+	while (++i < ac && av[i][0] == '-')
 	{
-		ft_putstr_fd("Malloc OK. \n", 2);
-		while (av[++i][0] == '-')
+		while (++j < ft_[i]))
 		{
-			printf("i = %d \n", i);
-			while (++j < ft_strlen(av[i]))
-			{
-				flag->all = (av[i][j] == 'a') ? TRUE : FALSE;
-				flag->list = (av[i][j] == 'l') ? TRUE : FALSE;
-				flag->rec = (av[i][j] == 'r') ? TRUE : FALSE;
-				flag->rev = (av[i][j] == 'R') ? TRUE : FALSE;
-				flag->time = (av[i][j] == 't') ? TRUE : FALSE;
-				printf("av[i][j] = %c : i = %d\n", av[i][j], (int)j);
-			}
-		j = 0;
-		ft_putstr_fd("j Reinitialized \n", 2);
+			if (av[i][j] == 'a')
+				flag->all = TRUE;
+			else if (av[i][j] == 'l')
+				flag->list = TRUE;
+			else if (av[i][j] == 'R')
+				flag->rec = TRUE;
+			else if (av[i][j] == 'r')
+				flag->rev = TRUE;
+			else if (av[i][j] == 't')
+				flag->time = TRUE;
+			else
+				ft_fail(flag);
 		}
+		j = 0;
 	}
-	printf("all = %d \n", flag->all);
-	printf("list = %d \n", flag->list);
-	printf("rec = %d \n", flag->rec);
-	printf("rev = %d \n", flag->rev);
-	printf("time = %d \n", flag->time);
+	if (flag->check == FALSE)
+		ft_fail(flag);
 	return (flag);
-}
-
-static int			ft_checkflag(int ac, char **av)
-{
-	int				check;
-	int				i;
-	size_t			j;
-
-	i = 0;
-	check = 1;
-	while (++i < ac)
-	{
-		j = 0;
-		if (av[i][0] == '-')
-		{
-			while (++j < ft_strlen(av[i]) && check)
-			{
-				check = (av[i][j] == 'a' || av[i][j] == 'l' || av[i][j] == 'R'||
-						av[i][j] == 'r' || av[i][j] == 't') ? 1 : 0;
-				if (!check)
-					ft_usage();
-			}
-		}
-	}
-	return (check);
 }
 
 int					main(int ac, char **av)
 {
-	if (!ft_checkflag(ac, av))
+	if(!(ft_parseflags(ac, av)))
 		return (1);
-	ft_parse_flags(av);
-	return (0);
+	else
+		return (0);
 }
