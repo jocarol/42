ef731c263afca36e1f8dcc5fd0406b37d8f263c3
Joris Carol <SkyWanker@MacBook-Pro-2.local>
2018-01-12 20:06:50 +0100

    Push d03

---
+++ projects/piscinePhp/d03/ex03/cookie_crisp.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_GET['action']))
+{
+    if (isset($_GET['name']))
+    {
+        if ($_GET['action'] == 'set')
+            setcookie($_GET['name'], $_GET['value'], time()+3600);
+        elseif ($_GET['action'] == 'get')
+            echo $_COOKIE[$_GET['name']];
+        elseif ($_GET['action'] == 'del')
+            setcookie($_GET['name'], '', -1);
+    }
+}
+?>
---
+++ projects/piscinePhp/d03/ex06/members_only.php
@@ -0,0 +1,11 @@
+<?php
+$img_str = base64_encode(file_get_contents("./../img/42.png"));
+if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "jaimelespetitsponeys")
+    echo "<html><body>\nBonjour ".ucfirst($_SERVER['PHP_AUTH_USER'])."<br />\n<img src='data:image/png;base64,$img_str'>\n</body></html>\n";
+else
+{
+    header('WWW-Authenticate: Basic realm="Espace membres"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+}
+?>
